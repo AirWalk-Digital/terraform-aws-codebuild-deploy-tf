bde3c7f701568d92c6be58e2755205671ed076df
vishbhalla <Vishal.m.Bhalla@gmail.com>
2019-07-04 12:44:21 +0100

    TF 0.12 upgrade

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  region  = "${var.region}"
+  region = var.region
 }
 
 data "terraform_remote_state" "state" {
   backend = "s3"
 
-  config {
-    bucket  = "${lookup(var.terraform_state, "bucket")}"
-    region  = "${var.region}"
-    key     = "${lookup(var.terraform_state, "key")}"
+  config = {
+    bucket  = var.terraform_state["bucket"]
+    region  = var.region
+    key     = var.terraform_state["key"]
     encrypt = true
   }
 }
@@ -22,13 +22,13 @@ data "aws_caller_identity" "current_account_id" {}
 ////////// CodeBuild Project
 
 module "codebuild_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=0.2.1"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  attributes = ["${compact(concat(var.attributes, list("codebuild", var.region)))}"]
-  delimiter  = "${var.delimiter}"
-  tags       = "${var.tags}"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git"
+  namespace  = var.namespace
+  stage      = var.stage
+  name       = var.name
+  attributes = ["codebuild", var.region]
+  delimiter  = var.delimiter
+  tags       = var.tags
 }
 
 resource "aws_s3_bucket" "artifcats" {
@@ -39,8 +39,8 @@ resource "aws_s3_bucket" "artifcats" {
 
 data "aws_iam_policy_document" "codebuild" {
   statement {
-    sid    = "TrustPolicy"
-    effect = "Allow"
+    sid     = "TrustPolicy"
+    effect  = "Allow"
     actions = ["sts:AssumeRole"]
 
     principals {
@@ -51,30 +51,39 @@ data "aws_iam_policy_document" "codebuild" {
 }
 
 resource "aws_iam_role" "codebuild" {
-  name               = "${module.codebuild_label.id}"
-  assume_role_policy = "${data.aws_iam_policy_document.codebuild.json}"
+  name               = module.codebuild_label.id
+  assume_role_policy = data.aws_iam_policy_document.codebuild.json
 }
 
 resource "aws_iam_role_policy_attachment" "codebuild" {
-  count      = "${length(var.codebuild_iam_policy_arns)}"
-  role       = "${aws_iam_role.codebuild.name}"
-  policy_arn = "${var.codebuild_iam_policy_arns[count.index]}"
+  count      = length(var.codebuild_iam_policy_arns)
+  role       = aws_iam_role.codebuild.name
+  policy_arn = var.codebuild_iam_policy_arns[count.index]
 }
 
 resource "aws_codebuild_project" "codebuild" {
-  name         = "${module.codebuild_label.id}"
-  description  = "${var.codebuild_project_description}"
-  service_role = "${aws_iam_role.codebuild.arn}"
+  name         = module.codebuild_label.id
+  description  = var.codebuild_project_description
+  service_role = aws_iam_role.codebuild.arn
 
   artifacts {
     type = "CODEPIPELINE"
   }
 
   environment {
-    compute_type         = "${var.codebuild_compute_type}"
-    image                = "${var.codebuild_image}"
-    type                 = "${var.codebuild_type}"
-    environment_variable = "${var.codebuild_env_vars}"
+    compute_type = var.codebuild_compute_type
+    image        = var.codebuild_image
+    type         = var.codebuild_type
+    dynamic "environment_variable" {
+      for_each = [for v in var.codebuild_env_vars: {
+        name = v.name
+        value = v.value
+      }]
+      content {
+        name  = environment_variable.value.name
+        value = environment_variable.value.value
+      }
+    }
   }
 
   source {
@@ -85,13 +94,13 @@ resource "aws_codebuild_project" "codebuild" {
 ////////// CodePipeline Pipeline
 
 module "codepipeline_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=0.2.1"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  attributes = ["${compact(concat(var.attributes, list("codepipeline", var.region)))}"]
-  delimiter  = "${var.delimiter}"
-  tags       = "${var.tags}"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git"
+  namespace  = var.namespace
+  stage      = var.stage
+  name       = var.name
+  attributes = ["codepipeline", var.region]
+  delimiter  = var.delimiter
+  tags       = var.tags
 }
 
 data "aws_iam_policy_document" "codepipeline" {
@@ -101,36 +110,36 @@ data "aws_iam_policy_document" "codepipeline" {
     actions = ["sts:AssumeRole"]
 
     principals {
-      type        = "Service"
+      type = "Service"
       identifiers = [
-        "codepipeline.amazonaws.com"
+        "codepipeline.amazonaws.com",
       ]
     }
   }
 }
 
 resource "aws_iam_role" "codepipeline" {
-  name               = "${module.codepipeline_label.id}"
-  assume_role_policy = "${data.aws_iam_policy_document.codepipeline.json}"
+  name               = module.codepipeline_label.id
+  assume_role_policy = data.aws_iam_policy_document.codepipeline.json
 }
 
 resource "aws_iam_role_policy_attachment" "codepipeline" {
-  count      = "${length(var.codepipeline_iam_policy_arns)}"
-  role       = "${aws_iam_role.codepipeline.name}"
-  policy_arn = "${var.codepipeline_iam_policy_arns[count.index]}"
+  count      = length(var.codepipeline_iam_policy_arns)
+  role       = aws_iam_role.codepipeline.name
+  policy_arn = var.codepipeline_iam_policy_arns[count.index]
 }
 
 // Github OAuth token stored in SSM parameter store:
 data "aws_ssm_parameter" "github_token" {
-  name = "${var.ssm_param_name_github_token}"
+  name = var.ssm_param_name_github_token
 }
 
 resource "aws_codepipeline" "codepipeline" {
-  name     = "${module.codepipeline_label.id}"
-  role_arn = "${aws_iam_role.codepipeline.arn}"
+  name     = module.codepipeline_label.id
+  role_arn = aws_iam_role.codepipeline.arn
 
   artifact_store {
-    location = "${aws_s3_bucket.artifcats.bucket}"
+    location = aws_s3_bucket.artifcats.bucket
     type     = "S3"
   }
 
@@ -146,10 +155,10 @@ resource "aws_codepipeline" "codepipeline" {
       output_artifacts = ["source_output"]
 
       configuration = {
-        Owner      = "${var.github_owner}"
-        Repo       = "${var.github_repo}"
-        Branch     = "${var.git_branch}"
-        OAuthToken = "${data.aws_ssm_parameter.github_token.value}"
+        Owner      = var.github_owner
+        Repo       = var.github_repo
+        Branch     = var.git_branch
+        OAuthToken = data.aws_ssm_parameter.github_token.value
       }
     }
   }
@@ -166,7 +175,7 @@ resource "aws_codepipeline" "codepipeline" {
       output_artifacts = ["build_output"]
       version          = "1"
       configuration = {
-        ProjectName = "${aws_codebuild_project.codebuild.name}"
+        ProjectName = aws_codebuild_project.codebuild.name
       }
     }
   }
--- variables.tf
+++ variables.tf
@@ -1,72 +1,73 @@
 variable "terraform_state" {
   description = "Terraform backend state setup for S3"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "region" {
   description = "region"
-  type        = "string"
+  type        = string
   default     = "eu-west-1"
 }
 
 variable "namespace" {
   description = "Namespace"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
-variable name {
+variable "name" {
   description = "Name (e.g. project name)"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
-variable stage {
+variable "stage" {
   description = "Stage (e.g. environment)"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
-variable attributes {
+variable "attributes" {
   description = "Additional attributes (e.g. `policy` or `role`)"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
-variable delimiter {
+variable "delimiter" {
   description = "Delimiter to be used between `name`, `namespace`, `environment`, etc."
-  type        = "string"
+  type        = string
   default     = "-"
 }
 
-variable tags {
+variable "tags" {
   description = "Tags"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "codebuild_project_description" {
   description = "Description of CodeBuild project"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "codebuild_iam_policy_arns" {
   description = "IAM Policy to be attached to role"
-  type        = "list"
-  default     = [
+  type        = list(string)
+  default = [
     "arn:aws:iam::aws:policy/AWSLambdaFullAccess",
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
     "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
     "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess",
+    "arn:aws:iam::aws:policy/IAMFullAccess",
   ]
 }
 
 variable "codepipeline_iam_policy_arns" {
   description = "IAM Policy to be attached to role"
-  type        = "list"
-  default     = [
+  type        = list(string)
+  default = [
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
     "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess",
   ]
@@ -74,7 +75,8 @@ variable "codepipeline_iam_policy_arns" {
 
 variable "codebuild_env_vars" {
   description = "A map of env vars to set in CodeBuild"
-  type        = "list"
+  #type        = object({ name=string, value=number })
+  type        = list(any)
   default = [
     {
       name  = "TF_VERSION"
@@ -83,48 +85,62 @@ variable "codebuild_env_vars" {
     {
       name  = "TF_ENV"
       value = "sandbox3"
+    },
+    {
+      name  = "TF_ACTION"
+      #value = "apply"
+      value = "destroy"
+    },
+    {
+      name  = "TF_IN_AUTOMATION"
+      value = "1"
+    },
+    {
+      name  = "TF_LOG"
+      value = "DEBUG"  # Available options: TRACE, DEBUG, INFO, WARN or ERROR
     }
   ]
 }
 
 variable "codebuild_compute_type" {
   description = "CodeBuild compute type"
-  type        = "string"
+  type        = string
   default     = "BUILD_GENERAL1_SMALL"
 }
 
 variable "codebuild_image" {
   description = "CodeBuild image"
-  type        = "string"
+  type        = string
   default     = "aws/codebuild/standard:2.0"
 }
 
 variable "codebuild_type" {
   description = "CodeBuild image type"
-  type        = "string"
+  type        = string
   default     = "LINUX_CONTAINER"
 }
 
 variable "github_owner" {
   description = "Github Repo owner/user"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "github_repo" {
   description = "Github repo name"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "git_branch" {
   description = "Git branch to build"
-  type        = "string"
+  type        = string
   default     = "master"
 }
 
 variable "ssm_param_name_github_token" {
   description = "The SSM parameter store, parameter name which stores the github ouath token"
-  type        = "string"
+  type        = string
   default     = "github_ouath_token_codepipeline"
 }
+
