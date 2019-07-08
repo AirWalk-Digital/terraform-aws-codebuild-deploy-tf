528a0b936f25e1d930edee27b9ec6a03ad67add5
vishbhalla <Vishal.m.Bhalla@gmail.com>
2019-07-08 10:20:38 +0100

    simplified outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "codebuild_project_name" {
   description = "CodeBuild project name"
-  value       = join("", aws_codebuild_project.codebuild.*.name)
+  #value       = join("", aws_codebuild_project.codebuild.*.name)
+  value       = aws_codebuild_project.codebuild.name
 }
 
 output "codepipeline_pipeline_name" {
   description = "CodePipeline pipeline name"
-  value       = join("", aws_codepipeline.codepipeline.*.name)
+  #value       = join("", aws_codepipeline.codepipeline.*.name)
+  value       = aws_codepipeline.codepipeline.name
 }
